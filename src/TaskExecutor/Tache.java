f00777fb8ba24c2421672476f9b1e63598f0975a
badinv <victor.badin@etu.univ-grenoble-alpes.fr>
2024-12-12 13:14:35 +0100

    Partie facultative terminée

--- src/TaskExecutor/Tache.java
+++ src/TaskExecutor/Tache.java
@@ -2,7 +2,14 @@ package src.TaskExecutor;
 
 public class Tache {
     public static void main(String[] args) {
-        System.out.println(args[0]);
+        System.out.println("Début tache " + args[0]);
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.out.println("Début tache " + args[0]);
     }
 
 }
